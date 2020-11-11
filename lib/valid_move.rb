1a75cf5ee1ff6ba4d1fc52401e4c79b64c629be0
Rachel Levinson <rachel.a.levinson@gmail.com>
2020-11-11 22:44:12 +0000

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -5,19 +5,15 @@ board = [" "," "," "," "," "," "," "," "," "]
 # define valid_move? - checks and returns true if move is valid
 def valid_move?(board,index)
   #accepts a board and index
-  if index.between?(0,8)
+  index.between?(0,8)
   #present on the game board
   ##if position_taken? is true
-    if position_taken? != true
-    end
-  end
 end
 
 #not already filled
 
 #use position_taken? as a helper method
-def position_taken?(board,index)
+def position_taken?(board, index)
   board[index] == "X" || board[index] == "O"
 end
-  #explore using two if statements or &&
   # "a false or nil return value for an invalid move will suffice"
