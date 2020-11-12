2490f9cad2e2e2f2d1d8cc811268f63d849ade03
Rachel Levinson <rachel.a.levinson@gmail.com>
2020-11-12 15:16:15 +0000

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -2,17 +2,12 @@ board = [" "," "," "," "," "," "," "," "," "]
 
   ###  if position_taken?(board,index != "X" || "O")
 
-# define valid_move? - checks and returns true if move is valid
 def valid_move?(board,index)
-  #accepts a board and index
-  index.between?(0,8)
-  #present on the game board
+!position_taken?(board,index) && index.between?(0,8)
   ##if position_taken? is true
 end
 
-#not already filled
 
-#use position_taken? as a helper method
 def position_taken?(board, index)
   board[index] == "X" || board[index] == "O"
 end
